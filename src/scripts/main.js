82372f1a8f197e137f62235e515e8b4e13557fbb
Kazathur92 <gabmin21@hotmail.com>
2021-04-19 14:37:33 -0500

    stuff for example proj

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,47 @@
 import ApiManager from "./apiManager";
 
-let apiManager = new ApiManager();
+let createPokemonInput = document.getElementById("createPokemonInput");
+let createPokemonButton = document.getElementById("createPokemonButton");
+let deletePokemonInput = document.getElementById("deletePokemonInput");
+let deletePokemonButton = document.getElementById("deletePokemonButton");
+let editPokemonButton = document.getElementById("editPokemonButton");
+let pokemonHeader = document.getElementById("pokemonShowcase");
 
-console.log(apiManager.getData());
+let newPokemon = {};
+let pokemonId = '';
+let apiManager = new ApiManager();
+let pokemonData;
+
+const changeInput = (event) => {
+    if (event.target.id === 'createPokemonInput') {
+        newPokemon[event.target.name] = event.target.value;
+    } else {
+        pokemonId = event.target.value;
+    }
+};
+
+
+createPokemonInput.addEventListener('change', changeInput);
+deletePokemonInput.addEventListener('change', changeInput);
+createPokemonButton.addEventListener('click', () =>  {apiManager.addPokemon(newPokemon)});
+deletePokemonButton.addEventListener('click', () =>  {apiManager.deletePokemon(pokemonId)});
+editPokemonButton.addEventListener('click', () => {apiManager.editPokemon(newPokemon, pokemonId)});
+
+
+let initialize = new Promise((resolve, reject) => {
+    apiManager.getPokemon().then(data => {
+        pokemonData = data;
+        resolve(pokemonData);
+    });
+});
+
+initialize.then((data) => {
+
+    pokemonData.forEach(pokemon => {
+        let newPokemonElement = document.createElement('p')
+        newPokemonElement.innerText = pokemon.name
+        pokemonHeader.appendChild(newPokemonElement);
+    })
+
+});
+
